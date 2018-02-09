f552db3dbc8d74c2af5569336416554ad623d32f
Jack Parmer <4letterfirstname@plot.ly>
2018-02-08 22:34:06 -0500

    make app prettier

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 import dash_colorscales
 import pandas as pd
 import cufflinks as cf
@@ -13,8 +14,9 @@ server = app.server
 df_lat_lon = pd.read_csv('lat_lon_counties.csv')
 df_lat_lon['FIPS '] = df_lat_lon['FIPS '].apply(lambda x: str(x).zfill(5))
 
-df_full_data = pd.read_csv('age_adjusted_death_rate.csv')
-df_full_data['"County Code"'] = df_full_data['"County Code"'].apply(lambda x: str(x).zfill(5))
+df_full_data = pd.read_csv('age_adjusted_death_rate_no_quotes.csv')
+df_full_data['County Code'] = df_full_data['County Code'].apply(lambda x: str(x).zfill(5))
+df_full_data['County'] = df_full_data['Unnamed: 0'] + ", " + df_full_data.County.map(str)
 
 YEARS = [2003, 2004, 2005, 2006, 2007, \
 		2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
@@ -26,6 +28,8 @@ BINS = ['0-2', '2.1-4', '4.1-6', '6.1-8', '8.1-10', '10.1-12', '12.1-14', \
 DEFAULT_COLORSCALE = ["#2a4858", "#265465", "#1e6172", "#106e7c", "#007b84", \
 	"#00898a", "#00968e", "#19a390", "#31b08f", "#4abd8c", "#64c988", \
 	"#80d482", "#9cdf7c", "#bae976", "#d9f271", "#fafa6e"]
+
+DEFAULT_OPACITY = 0.8
 
 #DEFAULT_COLORSCALE = reversed(DEFAULT_COLORSCALE)
 
@@ -52,7 +56,7 @@ app.layout = html.Div(children=[
 			value=min(YEARS),
 			marks={str(year): str(year) for year in YEARS},
 		),
-	], style={'width':800, 'margin':20}),
+	], style={'width':800, 'margin':25}),
 
 	html.Div([
 		dash_colorscales.DashColorscales(
@@ -60,8 +64,24 @@ app.layout = html.Div(children=[
 			colorscale=DEFAULT_COLORSCALE,
 			nSwatches=16,
 			fixSwatches=True
-		)
-	]),
+		),
+	], style={'display':'inline-block'}),
+
+	html.P('Map transparency:',
+		style={
+			'display':'inline-block', 
+			'verticalAlign': 'top',
+			'marginRight': '10px'
+		}
+	),
+
+	html.Div([
+		dcc.Slider(
+			id='opacity-slider',
+			min=0, max=1, value=DEFAULT_OPACITY, step=0.1,
+			marks={tick: str(tick)[0:3] for tick in np.linspace(0,1,11)},
+		),
+	], style={'width':400, 'display':'inline-block', 'marginBottom': 10}),
 
 	dcc.Graph(
 		id = 'county-choropleth',
@@ -71,13 +91,33 @@ app.layout = html.Div(children=[
 				lon = df_lat_lon['Longitude'],
 				text = df_lat_lon['Hover'],
 				type = 'scattermapbox'
+			),
+			layout = dict(
+				mapbox = dict(
+					layers = [],
+					accesstoken = mapbox_access_token,
+					style = 'light',
+					center=dict(
+						lat=38.72490,
+						lon=-95.61446,
+					),
+					pitch=0,
+					zoom=3.2
+				)
 			)
 		)
 	),
 
 	dcc.Graph(
 		id = 'selected-data',
-		figure = dict()
+		figure = dict(
+			data = [dict(x=0, y=0)],
+			layout = dict(
+				paper_bgcolor = '#F4F4F8',
+				plot_bgcolor = '#F4F4F8'
+			)
+		),
+		# animate = True
 	),
 
 	html.Div([
@@ -89,14 +129,15 @@ app.layout = html.Div(children=[
 	])
 ])
 
-app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+app.css.append_css({'external_url': 'https://codepen.io/plotly/pen/EQZeaW.css'})
 
 @app.callback(
-		dash.dependencies.Output('county-choropleth', 'figure'),
-		[dash.dependencies.Input('years-slider', 'value'),
-		dash.dependencies.Input('colorscale-picker', 'colorscale')])
-def display_map(year, colorscale):
-
+		Output('county-choropleth', 'figure'),
+		[Input('years-slider', 'value'),
+		Input('opacity-slider', 'value'),
+		Input('colorscale-picker', 'colorscale')],
+		[State('county-choropleth', 'figure')])
+def display_map(year, opacity, colorscale, figure):
 	cm = dict(zip(BINS, colorscale))
 
 	data = [dict(
@@ -106,7 +147,7 @@ def display_map(year, colorscale):
 		type = 'scattermapbox',
 		#selected = dict(marker = dict(opacity=1)),
 		#unselected = dict(marker = dict(opacity = 0)),
-		marker = dict(size=5, color='white', opacity=1)
+		marker = dict(size=5, color='white', opacity=0)
 	)]
 
 	annotations = [dict(
@@ -132,17 +173,22 @@ def display_map(year, colorscale):
 			)
 		)
 
+	if 'layout' in figure:
+		lat = figure['layout']['mapbox']['center']['lat']
+		lon = figure['layout']['mapbox']['center']['lon']
+		zoom = figure['layout']['mapbox']['zoom']
+	else:
+		lat = 38.72490,
+		lon = -95.61446,
+		zoom = 3.2
+
 	layout = dict(
 		mapbox = dict(
 			layers = [],
 			accesstoken = mapbox_access_token,
 			style = 'light',
-			center=dict(
-				lat=38.72490,
-				lon=-95.61446,
-			),
-			pitch=0,
-			zoom=3.2,
+			center=dict(lat=lat, lon=lon),
+			zoom=zoom
 		),
 		hovermode = 'closest',
 		margin = dict(r=0, l=0, t=0, b=0),
@@ -157,7 +203,7 @@ def display_map(year, colorscale):
 			source = base_url + str(year) + '/' + bin + '.geojson',
 			type = 'fill',
 			color = cm[bin],
-			opacity = 0.8
+			opacity = opacity
 		)
 		layout['mapbox']['layers'].append(geo_layer)
 
@@ -165,29 +211,36 @@ def display_map(year, colorscale):
 	return fig
 
 @app.callback(
-	dash.dependencies.Output('selected-data', 'figure'),
-	[dash.dependencies.Input('county-choropleth', 'selectedData')])
+	Output('selected-data', 'figure'),
+	[Input('county-choropleth', 'selectedData')])
 def display_selected_data(selectedData):
 	print('FIRE SELECTION')
 	if selectedData is None:
 		print('SelectedData is None')
-		return {}
+		return dict(
+			data = [dict(x=0, y=0)],
+			layout = dict(
+				title='Drag-select on the map',
+				paper_bgcolor = '#F4F4F8',
+				plot_bgcolor = '#F4F4F8'
+			)
+		)
 	pts = selectedData['points']
 	fips = [str(pt['text'].split('<br>')[-1]) for pt in pts]
 	for i in range(len(fips)):
 		if len(fips[i]) == 4:
 			fips[i] = '0' + fips[i]
 	print('FIPS', '\n', fips)
-	dff = df_full_data[df_full_data['"County Code"'].isin(fips)]
-	dff = dff.sort_values('"Year"')
+	dff = df_full_data[df_full_data['County Code'].isin(fips)]
+	dff = dff.sort_values('Year')
 	dff['Age Adjusted Rate'] = dff['Age Adjusted Rate'].str.strip('(Unreliable)')
 	print('DFF', '\n', dff.head()['Age Adjusted Rate'])
 	fig = dff.iplot(
 		kind = 'area',
-		x = '"Year"',
+		x = 'Year',
 		y = 'Age Adjusted Rate',
-		text = '"County"',
-		categories = '"County Code"',
+		text = 'County',
+		categories = 'County',
 		asFigure=True)
 
 	for i, trace in enumerate(fig['data']):
